68bfeabdc8abc9cd0357475cb48f53e0d113ece7
ludovic-simon <ludovic.simon82@gmail.com>
2014-11-18 15:02:27 +0100

    maj vues sans prefixe

---
+++ WebContent/controllers/accueil.js
@@ -0,0 +1,13 @@
+var accueilViewController = angular.module('accueilViewController', []);
+ 
+accueilViewController.controller('AccueilViewCtrl', ['$scope', '$http', '$location', '$route',
+function AccueilViewCtrl($scope, $http, $location, $route) {
+
+
+	$scope.init = function() {
+
+	};
+
+	$scope.init();
+
+}]);
